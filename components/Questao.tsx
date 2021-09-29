7863dbf217e131cbcff215d4bba694f08da2c0ca
Breno <bhfs.2908@gmail.com>
2021-09-29 19:42:43 -0300

    feat:add component resposta

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import QuestaoModel from "../model/questao";
 import styles from "../styles/Questao.module.css";
 import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 
 interface QuestaoProps {
   valor: QuestaoModel;
 }
 
 export default function Questao(props: QuestaoProps) {
-  const Questao = props.valor;
+  const questao = props.valor;
+
+  function renderizarRespostas() {
+    return questao.respostas.map((resposta, i) => {
+      return <Resposta valor={resposta} indice={i} letra="A" corLetra="#F2C866" key={i} />;
+    });
+  }
 
   return (
     <div className={styles.questao}>
-      <Enunciado texto={Questao.enunciado} />
+      <Enunciado texto={questao.enunciado} />
+      {renderizarRespostas()}
     </div>
   );
 }
