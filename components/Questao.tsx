f4f263b1a009f9900241d05a7e3c7ffb0b5396af
Breno <bhfs.2908@gmail.com>
2021-09-30 17:08:02 -0300

    feat:componente temporizador

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -3,17 +3,19 @@ import QuestaoModel from "../model/questao";
 import styles from "../styles/Questao.module.css";
 import Enunciado from "./Enunciado";
 import Resposta from "./Resposta";
+import Temporizador from "./Temporizador";
 
 const letras = [
-  {valor: "A", cor: "#F2C866"},
-  {valor: "B", cor: "#F266BA"},
-  {valor: "C", cor: "#85D4F2"},
-  {valor: "D", cor: "#BCE596"},
-]
+  { valor: "A", cor: "#F2C866" },
+  { valor: "B", cor: "#F266BA" },
+  { valor: "C", cor: "#85D4F2" },
+  { valor: "D", cor: "#BCE596" },
+];
 
 interface QuestaoProps {
   valor: QuestaoModel;
   respostaFornecida: (indice: number) => void;
+  tempoEsgotado: () => void;
 }
 
 export default function Questao(props: QuestaoProps) {
@@ -21,13 +23,23 @@ export default function Questao(props: QuestaoProps) {
 
   function renderizarRespostas() {
     return questao.respostas.map((resposta, i) => {
-      return <Resposta valor={resposta} indice={i} letra={letras[i].valor} corFundoLetra={letras[i].cor} key={i} respostaFornecida={props.respostaFornecida} />;
+      return (
+        <Resposta
+          valor={resposta}
+          indice={i}
+          letra={letras[i].valor}
+          corFundoLetra={letras[i].cor}
+          key={i}
+          respostaFornecida={props.respostaFornecida}
+        />
+      );
     });
   }
 
   return (
     <div className={styles.questao}>
       <Enunciado texto={questao.enunciado} />
+      <Temporizador duracao={10} tempoEsgotado={props.tempoEsgotado} />
       {renderizarRespostas()}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,17 @@ const questaoMock = new QuestaoModel(1, "melhor cor?", [
 ]);
 
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const [questao, setQuestao] = useState(questaoMock);
 
   function respostaFornecida(indice: number) {
-    console.log(indice)
-    setQuestao(questao.responderCom(indice))
+    console.log(indice);
+    setQuestao(questao.responderCom(indice));
+  }
+
+  function tempoEsgotado() {
+    if (questao.naoRespondida) {
+      setQuestao(questao.responderCom(-1));
+    }
   }
 
   return (
@@ -27,7 +33,7 @@ export default function Home() {
         height: "100vh",
       }}
     >
-      <Questao valor={questao} respostaFornecida={respostaFornecida}/>
+      <Questao valor={questao} respostaFornecida={respostaFornecida} tempoEsgotado={tempoEsgotado} />
     </div>
   );
 }
